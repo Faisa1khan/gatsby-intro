b980aaf51faccdc7ac014f9845d6dda8b13a52f0
faisa1khan <faisalkhan5464@gmial.com>
2019-09-21 14:27:57 +0530

    Final version

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Front Masters Gatsby Workshop',
+    description: 'A site we build together during a full day worshop',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
+        plugins: [{ resolve: 'gatsby-remark-images' }],
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'images',
+      },
+    },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: 'gatsbyjs',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disabled: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzernode: 'static',
+      },
+    },
+  ],
+};
